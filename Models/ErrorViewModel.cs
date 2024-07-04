36c5c442f6c89d9e09d348292db8d61782b86bc3
rudrayaduvanshi777 <rudrayaduvanshi777@gmail.com>
2024-07-04 14:03:42 +0530

    first time comit
    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NitinWebNewApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
