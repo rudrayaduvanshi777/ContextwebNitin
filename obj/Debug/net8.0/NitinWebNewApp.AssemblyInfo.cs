2afd463210376168cae5e1aded4d4f457d39a0d8
rudrayaduvanshi777 <rudrayaduvanshi777@gmail.com>
2024-08-30 22:57:29 +0530

    3rd commit
    3rd commit

--- obj/Debug/net8.0/NitinWebNewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NitinWebNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NitinWebNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebca09193d592e56b196ee11103f656b5d62734e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb95ceb9d4fafc5af930a5dd9270cba8bdfc0ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("NitinWebNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NitinWebNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
